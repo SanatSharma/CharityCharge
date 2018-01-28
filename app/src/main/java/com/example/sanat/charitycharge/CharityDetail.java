86c68df82ec915ebac9cb41e1880445e96674087
David Chen <davidzchen@utexas.edu>
2018-01-28 00:18:59 -0600

    more ui changes

--- app/src/main/java/com/example/sanat/charitycharge/CharityDetail.java
+++ app/src/main/java/com/example/sanat/charitycharge/CharityDetail.java
@@ -3,8 +3,11 @@ package com.example.sanat.charitycharge;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,7 +23,14 @@ public class CharityDetail extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_charity_detail);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
         Bundle b = getIntent().getExtras();
         int id = b.getInt("id");
@@ -38,9 +48,13 @@ public class CharityDetail extends AppCompatActivity {
         charityName.setText(charity.name);
         charityWebsite.setText(charity.website);
         charityDescription.setText(charity.description);
+    }
 
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_charity_list, menu);
+        return true;
     }
 
 }
