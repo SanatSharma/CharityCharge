f4b324f6ca6c5b1dd98e3c7fc593f363eb89e454
David Chen <davidzchen@utexas.edu>
2018-01-28 08:24:20 -0600

    more ui changes

--- app/src/main/java/com/example/sanat/charitycharge/WebSocketListener.java
+++ app/src/main/java/com/example/sanat/charitycharge/WebSocketListener.java
@@ -22,7 +22,7 @@ public class WebSocketListener extends okhttp3.WebSocketListener{
     }
 
     public static void start(){
-        Request request = new Request.Builder().url("http://6c734971.ngrok.io").build();
+        Request request = new Request.Builder().url("http://34ce6dfd.ngrok.io:3000").build();
         WebSocketListener listener = WebSocketListener.getInstance();
         WebSocket ws = client.newWebSocket(request, listener);
     }
